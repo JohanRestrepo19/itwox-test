a4b0a1d4f559a712bced98450a1b35437334ba57
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-05-18 20:16:05 -0500

    feat: add home button to dashboard navbar.

--- apps/next/app/dashboard/components/navbar.tsx
+++ apps/next/app/dashboard/components/navbar.tsx
@@ -1,11 +1,11 @@
 'use client'
 
+import Link from 'next/link'
 import { Button, H4, Nav, XStack } from 'tamagui'
 import { LogIn } from '@tamagui/lucide-icons'
 import { logout } from 'services/auth'
 
 export default function DashboardNavbar() {
-
   async function handleSignOut() {
     await logout()
   }
@@ -14,9 +14,15 @@ export default function DashboardNavbar() {
     <Nav backgroundColor={'lightblue'} opacity={0.95}>
       <XStack justifyContent="space-between" alignItems="center" p="$2">
         <H4 fontWeight="$4">Dashboard</H4>
-        <Button iconAfter={LogIn} onPress={handleSignOut}>
-          Sign Out
-        </Button>
+        <XStack gap="$4">
+          <Link href={'/home'}>
+            <Button>Home</Button>
+          </Link>
+
+          <Button iconAfter={LogIn} onPress={handleSignOut}>
+            Sign Out
+          </Button>
+        </XStack>
       </XStack>
     </Nav>
   )
